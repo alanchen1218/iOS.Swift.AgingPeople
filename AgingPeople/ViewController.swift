fbd1d18b200a88c4aec1adc6452d2fca42d69283
Alan Chen <alanchen1218@gmail.com>
2018-05-07 15:32:11 -0700

    Populating a UITableView with multiple elements,Using sub-titles with UITableViewCells,When app loads, display 12 rows,Names should be displayed in main label,{random number} years old displayed in detail label

--- AgingPeople/ViewController.swift
+++ AgingPeople/ViewController.swift
@@ -9,17 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var names = ["Alan","Sophie", "Jacky", "Aaron", "Andy", "Winton", "Alex", "Jonathan", "Darren", "Lorenzo"]
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.dataSource = self
+
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+            let cell  = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+            cell.textLabel?.text = names[indexPath.row]
+            cell.detailTextLabel?.text  = "\(arc4random_uniform(91) + 5)" + " years old"
+            return cell
+    }
+}
